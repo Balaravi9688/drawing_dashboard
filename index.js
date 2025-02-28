82edca9b08ec348ddfd484402d84ee85dee93e99
balavisai <balamurugan@visailabs.com>
2025-02-28 11:04:37 +0530

    routing all the request to html file

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 const server = http.createServer(app);
 
 // Initializing Websocket Server
